761492b9e1b9fc66ead53dee7065764520eae313
Rajveer Singh <117706744+rajveersinghcse@users.noreply.github.com>
2023-08-09 07:45:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Importing libraries-----------------------------------------------------------------------------------------
-import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
+import streamlit as st
 import numpy as np
 from scipy import stats
 from sklearn.ensemble import RandomForestRegressor
